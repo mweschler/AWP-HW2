57eca946e80a750e60d7400f95bae5dc43a6b734
Michael Weschler <mweschler@gmail.com>
2013-10-29 14:35:14 -0400

    Added Shape enumeration

---
+++ Homework2/Homework2/ShapeEnum.cs
@@ -0,0 +1,12 @@
+﻿//Enumeration for Shape types. So important it deserves its own file. Bit of diva really...
+
+namespace Homework2
+{
+
+    enum ShapeEnum
+    { 
+        RECTANGLE,
+        ELLIPSE,
+        TRIANGLE
+    };
+}
