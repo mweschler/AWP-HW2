5898811f7deb0a3b1cc9f593ac305435959eb83e
Michael Weschler <mweschler@gmail.com>
2013-10-29 14:16:28 -0400

    Oath Dialog Completed

---
+++ Homework2/Homework2/OathDialog.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Homework2
+{
+    partial class OathDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.oathbox1 = new HW1ContLib.Oathbox();
+            this.innerPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // innerPanel
+            // 
+            this.innerPanel.Controls.Add(this.oathbox1);
+            this.innerPanel.Size = new System.Drawing.Size(502, 185);
+            // 
+            // oathbox1
+            // 
+            this.oathbox1.BackColor = System.Drawing.Color.Transparent;
+            this.oathbox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.oathbox1.Location = new System.Drawing.Point(0, 0);
+            this.oathbox1.Name = "oathbox1";
+            this.oathbox1.Size = new System.Drawing.Size(502, 185);
+            this.oathbox1.TabIndex = 0;
+            // 
+            // OathDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(502, 324);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "OathDialog";
+            this.Text = "Oath Dialog";
+            this.innerPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private HW1ContLib.Oathbox oathbox1;
+    }
+}
--- Homework2/Homework2/OathDialog.cs
+++ Homework2/Homework2/OathDialog.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Homework2
 {
-    public partial class Form1 : Form
+    public partial class OathDialog : HW1ContLib.BaseDialogBox
     {
-        public Form1()
+        public OathDialog()
         {
             InitializeComponent();
         }
