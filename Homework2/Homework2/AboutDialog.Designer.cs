64017bd7324947f4a3ccfcd972f6b4c65d11dafc
Michael Weschler <mweschler@gmail.com>
2013-10-29 14:30:47 -0400

    About box completed

---
+++ Homework2/Homework2/AboutDialog.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Homework2
+{
+    partial class AboutDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutDialog));
+            this.aboutDescription = new System.Windows.Forms.Label();
+            this.innerPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // innerPanel
+            // 
+            this.innerPanel.Controls.Add(this.aboutDescription);
+            this.innerPanel.Size = new System.Drawing.Size(502, 185);
+            // 
+            // aboutDescription
+            // 
+            this.aboutDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.aboutDescription.Location = new System.Drawing.Point(0, 0);
+            this.aboutDescription.Name = "aboutDescription";
+            this.aboutDescription.Size = new System.Drawing.Size(502, 185);
+            this.aboutDescription.TabIndex = 0;
+            this.aboutDescription.Text = resources.GetString("aboutDescription.Text");
+            this.aboutDescription.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AboutDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(502, 324);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutDialog";
+            this.Text = "About";
+            this.innerPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label aboutDescription;
+    }
+}
---
+++ Homework2/Homework2/AboutDialog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Homework2
+{
+    public partial class AboutDialog : HW1ContLib.BaseDialogBox
+    {
+        public AboutDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
